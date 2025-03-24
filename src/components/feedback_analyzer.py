e78dc6dc615aa618a6c4058bb67b11f599ce3ac0
aryagokh <arya.gokhale1@gmail.com>
2025-03-25 03:25:43 +0530

    add feedback_analyzer and improvement_suggestor components

---
+++ src/components/feedback_analyzer.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import google.genai as genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+
+def file_to_text(file_path, review_column_name='reviews'):
+    if str(file_path).endswith('csv'):
+        data = pd.read_csv(file_path)
+    elif str(file_path).endswith('xlsx'):
+        data = pd.read_excel(file_path)
+    elif str(file_path).endswith('json'):
+        data = pd.read_json(file_path)
+    else:
+        print('Invalid file type. Please upload csv, excel or json files!!!')
+        return -1
+    collective_response = ''
+    for i in range(len(data)):
+        collective_response = collective_response + '\n' + data[review_column_name][i]
+    return collective_response
+
+def create_analysing_prompt(data, additional_info=None):
+    prompt = f'''
+    You are an expert in analyzing the reviews.
+    Given any set of reviews, you analyze them perfectly.
+    You list the issues listed inside the reviews in the form of bullet points.
+    If no issue is present, just tell 'No issues'
+    Do not add filler text. Just output the bullet points if issue is present.
+    And strictly do not add **headers like this**.
+
+    This is the data on which you have to perform analyzing process:
+    {data}
+    '''
+    if additional_info is not None:
+        prompt = prompt + '\n' + f'Here is some additional information about the same : {additional_info}'
+
+    return prompt
+
+def analyze_feedback(file_path, review_column_name='reviews', additional_info=None):
+    try:
+        collective_response = file_to_text(file_path)
+        client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-lite',
+            contents=create_analysing_prompt(collective_response)
+        )
+        return response.candidates[0].content.parts[0].text
+    except Exception as e:
+        print(e)
+        return -1
+
+
+# if __name__ == '__main__':
+#     print(analyze_feedback(
+#         file_path='data/raw/documents/Issues_DS.xlsx'
+#     ))
---
+++ src/components/improvement_suggestor.py
@@ -0,0 +1,65 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv('OPENROUTER_API_KEY')
+)
+
+
+def create_improvement_suggestion_prompt(background, issues, additional_info=None, whom=None):
+    system_prompt = f'''
+    You are an expert in improvement suggestions of all the content.
+    You can use your available knowledge or search on internet, you decide.
+    Respond with strictly only resolvements of the issues.
+    No filler words and headers.
+    Format for all issues and resolution should be:
+    * Issue:
+    * Resolution:
+    '''
+    user_prompt = f'''
+    This is some background for your prerequisite knowledge:
+    {background}
+
+    These are the issues which are the input:
+    {issues}
+    '''
+    if additional_info is not None:
+        if whom=='system':
+            system_prompt = system_prompt + '\n' + f'''{additional_info}'''
+        elif whom=='user':
+            user_prompt = user_prompt + '\n' + f'''Also, consider this additional information \n{additional_info} '''
+        else:
+            print('No additional info given to anyone. Stop if you believe its a mistake!!')
+    return system_prompt, user_prompt
+
+
+def suggest_improvements(system_prompt, user_prompt):
+    try:
+        response = client.chat.completions.create(
+            model='deepseek/deepseek-r1-zero:free',
+            messages = [
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": user_prompt},
+                ]
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(e)
+        return -1
+    
+
+if __name__ == '__main__':
+    issues = f'''
+    *   Deployment process took longer than expected.
+    *   Documentation provided for integration was incomplete.
+    *   Pricing was on the higher side.
+    *   Some model predictions were difficult to interpret.
+    '''
+
+    background = 'AI ML Big Data Company'
+    system_prompt, user_prompt = create_improvement_suggestion_prompt(background, issues)
+    print(suggest_improvements(system_prompt, user_prompt))
+
