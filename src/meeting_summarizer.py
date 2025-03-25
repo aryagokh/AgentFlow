ce6fd60f68f007d99ee314712ffb1eff6149a8f5
aryagokh <arya.gokhale1@gmail.com>
2025-03-25 13:20:52 +0530

    update meeting summarizer

--- src/meeting_summarizer.py
+++ src/meeting_summarizer.py
@@ -2,10 +2,8 @@ from components.video_to_audio import convert_video_to_audio_moviepy
 from components.audio_to_text import audio_to_text
 from components.summary_generator import summary_generator
 from components.mail_handling import handle_mail
-# Mail them
 from fastapi import FastAPI, UploadFile, File
 import uvicorn
-import sys
 import os
 
 app = FastAPI()
@@ -33,5 +31,3 @@ async def upload_video(file: UploadFile = File(...), send_mail: str=None) -> str
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-
-
