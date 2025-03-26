74b4f2cf7234cd629f6500b4787fff5d9e11eeed
aryagokh <arya.gokhale1@gmail.com>
2025-03-26 10:49:36 +0530

    add realtime_price_generator

---
+++ src/components/realtime_price_generator.py
@@ -0,0 +1,62 @@
+import google.genai as genai 
+from google.genai.types import Tool, GoogleSearch, GenerateContentConfig
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def create_price_generation_prompt(item, additional_info=None, website=None):
+    prompt = f'''
+    You are an expert in scraping the internet and providing **real time retail prices**.
+    Everytime you get request for any item's price, you use google search tool provided and return the price of the item.
+
+    Please don't use any filler words. Also, no **highlighted headings and texts**.
+    Return strictly only
+    ```json
+    {{
+    food_item_1 : price,
+    ...
+    }}```
+
+    Here is the item:
+    {item}
+    '''
+    if website is not None:
+        prompt = prompt + f'''
+        Try to search the required rel time information here:
+        {website}    
+        '''
+    if additional_info is not None:
+        prompt = prompt + f'''
+        Here is some additional things to consider:
+        {additional_info}
+        '''
+
+    return prompt
+
+
+def generate_price(item, addtional_info=None, website=None):
+    client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+    google_search_tool = Tool(
+        google_search=GoogleSearch()
+    )
+    response = client.models.generate_content(
+    model='gemini-2.0-flash',
+    contents=create_price_generation_prompt(item, 
+                                            additional_info=addtional_info,
+                                            website=website),
+    config=GenerateContentConfig(
+        tools=[google_search_tool],
+        response_modalities=['TEXT']
+    )
+    )
+    return response.candidates[0].content.parts[0].text
+
+
+# if __name__ == '__main__':
+#     response = generate_price(
+#         item=['Society Tea (100g)', 'Cow Milk (1/2 litre)', 'Sugar (100g)', 'Ginger(100g)'],
+#         addtional_info='The information should be based on Mumbai.',
+#         website=['zepto.com', 'blinkit.com']
+#     )
+
+#     print(response)
